d5b049d74ebfdc26ef291007f9c99dab4d380735
Zevik <monnatyz@gmail.com>
2022-01-06 23:45:18 +0700

    course count

--- src/app/controllers/PersonalController.js
+++ src/app/controllers/PersonalController.js
@@ -3,24 +3,32 @@ const CourseModel = require('../models/Course');
 class PersonalController {
   // [GET] /personal/stored-courses
   storedCourses(req, res, next) {
-    CourseModel.find({})
-      .lean()
-      .then((courses) => {
+    Promise.all([
+      CourseModel.countDocuments().lean(),
+      CourseModel.find({}).lean(),
+    ])
+      .then(([courseNum, courses]) => {
         res.render('personal/stored-courses', {
+          courseNum,
           courses,
         });
-      });
+      })
+      .catch(next);
   }
 
   // [GET] /personal/stored-courses/deleted
   storedCoursesDeleted(req, res, next) {
-    CourseModel.findDeleted({})
-      .lean()
-      .then((courses) => {
+    Promise.all([
+      CourseModel.countDocumentsDeleted().lean(),
+      CourseModel.findDeleted({}).lean(),
+    ])
+      .then(([courseNum, courses]) => {
         res.render('personal/stored-courses-deleted', {
+          courseNum,
           courses,
         });
-      });
+      })
+      .catch(next);
   }
 }
 
